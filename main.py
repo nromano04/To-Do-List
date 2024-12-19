d61041e912ad19b15d034503e9099bb5c812c7ea
nromano04 <157419141+nromano04@users.noreply.github.com>
2024-12-19 14:28:20 -0500

    Update main.py
    Replaced pycharm header with personal comment header

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+#To-do List Application
+#Author: Nick Romano
 
 def add_task(task_list):
     task_name = input("Enter the task: ")
